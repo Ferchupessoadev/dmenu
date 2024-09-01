bace3a25ae31efe682d4fa871edbd029ac64eb9f
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-09-01 01:41:46 -0300

    Update config

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 700;                    /* minimum width when centered */    
-static const unsigned int alpha = 0xcc;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#0f101a" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
@@ -23,7 +23,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeOut] = { OPAQUE, alpha },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines      = 15;
 
 /*
  * Characters not considered part of a word while deleting words
